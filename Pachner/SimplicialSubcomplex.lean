8c1a34522a0af65c05ea655be866ad1db3e6da40
gc974517 <gc974517@ohio.edu>
2025-08-19 13:12:29 -0400

    #9 Remove s in X.faces assumption from stars.

--- Pachner/SimplicialSubcomplex.lean
+++ Pachner/SimplicialSubcomplex.lean
@@ -527,10 +527,10 @@ by
 def star
     (X : AbstractSimplicialComplex E)
     (s : Finset E)
-    (s_in_X : s ∈ X.faces)
     -- I am slightly concerned that this is not required for the proofs so something might be wrong here
     -- even if it's not required in the proofs, it probably still makes sense to have it
     -- since it _is_ part of the ''mathematical'' definition
+    -- The point was to avoid the empty cpx being ∅ instead of {∅}. This is obviously obsolete now.
   : AbstractSimplicialComplex E :=
     AbstractSimplicialComplex.mk ({t ∈ X.faces | s ∪ t ∈ X.faces})
     (by
@@ -559,8 +559,7 @@ instance star.fintype
     (X : AbstractSimplicialComplex E)
     [Fintype X.faces]
     (s : Finset E)
-    (s_in_X : s ∈ X.faces)
-  : Fintype (star X s s_in_X).faces :=
+  : Fintype (star X s).faces :=
 by
   unfold _root_.star
   dsimp only [AbstractSimplicialComplex.faces]
@@ -574,8 +573,7 @@ by
 theorem star_subcomplex_simplices
     (X : AbstractSimplicialComplex E)
     (s t : Finset E)
-    (s_in_X : s ∈ X.faces)
-  : t ∈ (St(X, s) s_in_X).faces → t ∈ X.faces :=
+  : t ∈ St(X, s).faces → t ∈ X.faces :=
 by
   intro t_in_star
   simp only [_root_.star, Set.mem_sep_iff] at t_in_star
@@ -585,8 +583,8 @@ by
 theorem star_subcomplex
     (X : AbstractSimplicialComplex E)
     (s : Finset E)
-    (s_in_X : s ∈ X.faces)
-  : IsSubcomplex (St(X, s) s_in_X) X := -- something with '⊆' notation seems to be a problem again
+  : IsSubcomplex St(X, s) X := -- something with '⊆' notation seems to be a problem again
+                               -- You have to call the explicit "AbstractSimplicialComplex.instHadSubset" or equivalent.
 by
   simp only [IsSubcomplex, Set.subset_def]
   intro t
@@ -600,12 +598,12 @@ theorem star_iso
     (f : SimplicialMap X Y)
     (s_in_X : s ∈ X.faces)
     (t_in_Y : t ∈ Y.faces)
-    (f_iso : IsSimplicialIso f) : Finset.image f.map s = t → St(X, s) s_in_X ≅ St(Y, t) t_in_Y :=
+    (f_iso : IsSimplicialIso f) : Finset.image f.map s = t → St(X, s) ≅ St(Y, t) :=
   by
   intro fs_eq_t
   unfold IsSimpliciallyIso
-  have f_simp : IsSimplicialMap (St(X, s) s_in_X) (St(Y, t) t_in_Y) f.map :=
-    by
+  have f_simp : IsSimplicialMap St(X, s) St(Y, t) f.map :=
+  by
     simp only [IsSimplicialMap, _root_.star, Set.mem_sep_iff]
     intro u u_in_X_star
     choose u_in_X su_in_X using u_in_X_star
@@ -643,8 +641,8 @@ theorem star_iso
       use s
     specialize gf_id x_in_X
     assumption
-  have g_simp : IsSimplicialMap (St(Y, t) t_in_Y) (St(X, s) s_in_X) g.map :=
-    by
+  have g_simp : IsSimplicialMap St(Y, t) St(X, s) g.map :=
+  by
     simp only [IsSimplicialMap, _root_.star, Set.mem_sep_iff]
     intro u u_in_Y_star
     choose u_in_Y tu_in_Y using u_in_Y_star
@@ -663,15 +661,15 @@ theorem star_iso
   constructor
   · intro x x_in_star
     have x_in_X : x ∈ X.vertices :=
-      by
-      apply is_subcomplex_vertices X (St(X, s) s_in_X) (star_subcomplex X s s_in_X)
+    by
+      apply is_subcomplex_vertices X St(X, s) (star_subcomplex X s)
       assumption
     specialize gf_id x_in_X
     assumption
   · intro x x_in_star
     have x_in_Y : x ∈ Y.vertices :=
-      by
-      apply is_subcomplex_vertices Y (St(Y, t) t_in_Y) (star_subcomplex Y t t_in_Y)
+    by
+      apply is_subcomplex_vertices Y St(Y, t) (star_subcomplex Y t)
       assumption
     specialize fg_id x_in_Y
     assumption
